554f302f90fa426733a7c6148b72d5d8449d69eb
amardeep <mauryaamardeep5264@gmail.com>
2026-03-05 01:00:45 +0530

    first commit

---
+++ src/day2/Test.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to full stack development");
+		System.out.println("Java Full stcak development !");
+
+	}
+
+}
