b1522f199f6d084799141e521880b2f43613fcdd
amardeep <mauryaamardeep5264@gmail.com>
2026-03-07 00:28:34 +0530

    quetsions

---
+++ src/day2/q5.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class q5 {
+
+	public static void main(String[] args) {
+		int n = 4;
+		
+		for(int i = 1; i <= n ; i++) {
+			for(int j = 1; j <= n-i;j++) {
+				System.out.print(" ");
+			}
+			for (int p = 1; p <=i; p++) {
+			
+				System.out.print("*");
+			}
+			System.out.println();
+			
+			}
+	
+		}
+		
+
+	}
+
+
